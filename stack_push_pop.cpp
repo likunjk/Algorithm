36105c773c489a06efb4ff91ab369231a48e9f21
Kun Lee <likunjk@gmail.com>
2014-09-11 23:19:24 +0800

    Update stack_push_pop.cpp

--- stack_push_pop.cpp
+++ stack_push_pop.cpp
@@ -4,51 +4,52 @@
 #include<queue>
 #include<vector>
 #include<stack>
-#include<map>
 using namespace std;
 
 #define N 100009
 
-//可惜九度不支持unordered_map，不然的话效率更高
-map<int, int> ump;
 
-int data[N];
+int data_in[N];
+int data_out[N];
 stack<int> st;
 
-//本道题就是PAT上1051判断出栈序列是否合法的加强版，思路是一样的，O(n)的时间复杂度,前提是用unordered_map
+//本道题就是PAT上判断出栈序列是否合法的加强版，思路是一样的，O(n)的时间复杂度，可以不借助unordered_map
 //测试链接：http://ac.jobdu.com/problem.php?pid=1366
 int main(void)
 {
-	int i,n,tt;
+	int i,j,n,tt;
 	while(scanf("%d",&n)==1)
 	{
 		while(!st.empty())
 			st.pop();
-		ump.clear();
 
 		for(i=0; i<n; ++i)
-		{
-			scanf("%d", &data[i]);
-			ump[data[i]] = i;
-		}
-		int pos = -1;
-		int index;
+			scanf("%d", &data_in[i]);
+		for(i=0; i<n; ++i)
+			scanf("%d", &data_out[i]);
+
+		int pos = -1;	//已经出栈元素的最大下标, 初始为-1，表示还没有元素出栈
 		int state = 0;
 		for(i=0; i<n; ++i)
 		{
-			scanf("%d", &tt);
-			index = ump[tt];
-			if(index > pos)
-			{
-				for(int j=pos+1; j<=index; ++j)
-					st.push(data[j]);
-				pos = index;
+			//当栈不为空，且栈顶元素等于出栈值时，直接出栈
+			if(st.size()>0 && st.top()==data_out[i])
 				st.pop();
-			}
 			else
 			{
-				if(st.top() != tt)
+				//否则，要想是合法的出栈序列，则必须在后面的入栈序列中找到data_out[i]
+				for(j=pos+1; j<n; ++j)
+				{
+					st.push(data_in[j]);
+					if(data_in[j] == data_out[i])
+						break;
+				}
+				pos = j;
+				if(j==n)	//说明没有找到
+				{
 					state = 1;
+					break;
+				}
 				else
 					st.pop();
 			}
