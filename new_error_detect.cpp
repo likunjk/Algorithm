b5fcd3ba1878d1d618b113a4a7d053eefef1c625
Kun Lee <likunjk@gmail.com>
2014-09-29 16:42:20 +0800

    Create new_error_detect.cpp

---
+++ new_error_detect.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+/* 功能: 测试当new失败时究竟是抛出异常还是返回空指针 */
+void testExceptionOrNull(size_t size)
+{
+	int *p;
+	while(1)
+	{
+		try
+		{
+			p = new int[size];
+		}catch ( const bad_alloc& e) {
+			cout<<"testExceptionOrNull: Throw Exception\n";
+			return;
+		}
+		if(p==NULL)
+		{
+			cout<<"testExceptionOrNull: p is NULL\n";
+			return;
+		}
+	}
+}
+
+
+/* 功能: 测试std:noexception */
+void testNothrow(size_t size)
+{
+	int *p;
+	while(1)
+	{
+		try
+		{
+			p = new(std::nothrow) int[size];
+		}catch ( const bad_alloc& e) {
+			cout<<"testExceptionOrNull: Throw Exception\n";
+			return;
+		}
+		if(p==NULL)
+		{
+			cout<<"testNothrow: p is NULL\n";
+			return;
+		}
+	}
+}
+
+int main(void)
+{	
+	testExceptionOrNull(0xfffffff);	//Throw Exception
+
+	testNothrow(0xfffffff);	//testNothrow: p is NULL
+
+	return 0;
+}
+
